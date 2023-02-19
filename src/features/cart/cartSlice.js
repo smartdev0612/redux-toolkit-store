df192056c09690147a9b9fb18f5574332b811335
Smart Dev <danieldev0612@gmail.com>
2023-02-18 20:12:28 -0800

    create cart reducer

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,6 +10,13 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = []
+    },
+  },
 })
+
+export const { clearCart } = cartSlice.actions
 
 export default cartSlice.reducer
